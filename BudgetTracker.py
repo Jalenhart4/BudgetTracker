f283ea0e9d51e3c5681f08c2cdb9b2e910d68626
Jalenhart4 <Jalenhart4@users.noreply.github.com>
2022-08-04 22:24:52 -0500

    Budget Tracker w/Google Sheets API

--- BudgetTracker.py
+++ BudgetTracker.py
@@ -1,6 +1,7 @@
 import csv
 import gspread
 import datetime
+import time
 
 # Get Current Month ->
 # month_num = str(datetime.datetime.now().month)
@@ -21,36 +22,49 @@ bills = {
 }
 
 transactions = []
-for file in files:
-    with open(file, mode='r') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        for row in csv_reader:
 
-            date = row[0]
-            amount = float(row[1])
-            description = row[4]
-            category = 'Other'
 
-            # Search for commonly occurring bills in each bills description
-            # and assign them a category, default category = 'other'.
-            for bill_type, bill_names in bills.items():
-                for bill_name in bill_names:
-                    if bill_name in description:
-                        category = bill_type
+def bill_reader(finance_files, common_bills):
+    for file in finance_files:
+        with open(file, mode='r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for csv_row in csv_reader:
 
-            # Check bill description to determine Checking or Credit bill
-            if "CARD 5748" in description:
-                account_type = "Checking"
-            else:
-                account_type = "Credit"
+                date = csv_row[0]
+                amount = float(csv_row[1])
+                description = csv_row[4]
+                category = 'Other'
 
-            # Add transaction to list
-            transaction = [date, amount, account_type, category, description]
-            transactions.append(transaction)
+                # Search for commonly occurring bills in each bills description
+                # and assign them a category, default category = 'other'.
+                for bill_type, bill_names in common_bills.items():
+                    for bill_name in bill_names:
+                        if bill_name in description:
+                            category = bill_type
 
-for row in transactions:
-    print(row)
+                # Check bill description to determine Checking or Credit bill
+                if "CARD 5748" or "Online Transfer" or "Interest charge on purchases" in description:
+                    account_type = "Checking"
+                else:
+                    account_type = "Credit"
 
-# Connect to Google Spreadsheets 
-sa = gspread.service_account()
-sh = sa.open("Personal Finances")
+                # Add transaction to list
+                transaction = [date, amount, account_type, category, description]
+                transactions.append(transaction)
+    return transactions
+
+
+if __name__ == "__main__":
+
+    rows = bill_reader(files, bills)
+    for row in rows:
+        print(row)
+
+    # Connect to Google Spreadsheets
+    sa = gspread.service_account()
+    sheet = sa.open("Personal Finances")
+    worksheet = sheet.worksheet(f"{Month}")
+
+    for row in rows:
+        worksheet.insert_row([row[0], row[4], row[3], row[1], row[2]], 7)
+        time.sleep(2)
